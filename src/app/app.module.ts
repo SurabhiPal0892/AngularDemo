b8ddb98de259ae15df7ea23b19ba6a0c1333e0a6
SurabhiPal0892 <palsurabhi.0892@gmail.com>
2021-06-14 22:10:47 +0530

    ecommerce

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { UiDesignComponent } from './ui-design/ui-design.component';
 import { EcommerceComponent } from './ecommerce/ecommerce.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule,AppRoutingModule],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   declarations: [AppComponent, UiDesignComponent, EcommerceComponent],
   bootstrap: [AppComponent]
 })
