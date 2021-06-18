98a8d5318f0d15372c0597846668256b2b13e4b5
SurabhiPal0892 <palsurabhi.0892@gmail.com>
2021-06-19 02:49:18 +0530

    5 files modified

--- src/app/ecommerce/ecommerce.component.ts
+++ src/app/ecommerce/ecommerce.component.ts
@@ -9,14 +9,31 @@ import { products_sample_data } from '../data';
 export class EcommerceComponent implements OnInit {
   products = [];
   layout = 'Grid';
+  productsDisplay: any[];
   constructor() {}
   ngOnInit() {
     this.products = products_sample_data;
+    this.productsDisplay = this.products;
   }
   switchToGridLayout() {
     this.layout = 'Grid';
   }
   switchToListLayout() {
     this.layout = 'List';
+  }
+
+  getSelectedPrice(event) {
+    let price = event.target.value;
+    if (price < 0) {
+      return;
+    } else {
+      let range = price.split('-');
+      let p = this.products.filter(p => {
+        if (p.price >= range[0] && p.price <= range[1]) {
+          return true;
+        }
+      });
+      this.productsDisplay = p;
+    }
   }
 }
